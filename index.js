bebc65f527f3d571d926fe1c31cdc73b50f64e36
Srinevasan22 <sothynathans@gmail.com>
2024-11-20 10:11:51 +0000

    git push

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.get('/', (req, res) => {
     res.send('Pistol Tracking App Backend');
 });
 
-// Start the server - test
+// Start the server - test replit git push 2
 const PORT = 3030;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
