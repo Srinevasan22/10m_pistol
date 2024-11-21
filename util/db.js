391bd8fb78f675b9013e72948c04b824f24ee5c2
Srinevasan22 <sothynathans@gmail.com>
2024-11-21 08:41:20 +0000

    pistol

--- util/db.js
+++ util/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Set the strictQuery setting to true to prepare for Mongoose 7
+mongoose.set('strictQuery', true);
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    // Updated connection string to use 127.0.0.1 instead of localhost
+    const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/your-database-name';
+    await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
