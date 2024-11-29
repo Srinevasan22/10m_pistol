6122f03612f9c0e24389e12b2ddce9d49c6eb958
Srinevasan22 <sothynathans@gmail.com>
2024-11-29 21:06:01 +0000

    pistol

--- model/session.js
+++ model/session.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const sessionSchema = new mongoose.Schema({
     date: {
@@ -23,4 +23,4 @@ const sessionSchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model('Session', sessionSchema);
+export const Session = mongoose.model('Session', sessionSchema);
