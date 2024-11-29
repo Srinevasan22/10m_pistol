4264d092e38d2bf3418a49dd206cdbf158210f17
Srinevasan22 <sothynathans@gmail.com>
2024-11-29 20:19:22 +0000

    pistol

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './db.js';
-import sessionRoutes from './routes/sessionRoutes.js';
-import shotRoutes from './routes/shotRoutes.js';
+import connectDB from './util/db.js';
+import sessionRoutes from './route/sessionRoutes.js';
+import shotRoutes from './route/shotRoutes.js';
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3030;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
 // Connect to MongoDB
 connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Parse incoming JSON requests
 
 // Routes
 app.use('/api/sessions', sessionRoutes);
 app.use('/api/shots', shotRoutes);
 
-// Default route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the 10m Pistol API' });
-});
-
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
