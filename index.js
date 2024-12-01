62cd12f6e8f87d08c6cec28e57467d3f071a49e6
Srinevasan22 <sothynathans@gmail.com>
2024-12-01 16:12:51 +0000

    pistol

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json()); // Parse incoming JSON requests
 app.use('/api/sessions', sessionRoutes);
 app.use('/api/shots', shotRoutes);
 
-// Start the server - Test 4
+// Start the server - Test 5
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
